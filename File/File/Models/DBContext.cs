a531ce6c118f004d233d087c1e785584881c8282
ArinaBrs <tukovka@list.ru>
2019-05-28 11:51:37 +0300

    Download

---
+++ File/File/Models/DBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace File.Models
+{
+    public class DBContext : DbContext
+    {
+        public DBContext()
+        { }
+
+        public DBContext(DbContextOptions<DBContext> options)
+        : base(options)
+        { }
+
+        public DbSet<FileModel> Files { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFProviders.InMemory;Trusted_Connection=True;ConnectRetryCount=0");
+            }
+        }
+    }
+}
---
+++ File/File/Models/FileModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace File.Models
+{
+    public class FileModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+    }
+}
